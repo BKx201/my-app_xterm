bb5034a1aa5e85f193211eafcb903099a83dfef2
zhangsan <759824016@qq.com>
2020-04-20 17:06:45 +0800

    'first'

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { Terminal } from 'xterm';
+import { AttachAddon } from 'xterm-addon-attach';
+import { SearchAddon } from 'xterm-addon-search';
+import { FitAddon } from 'xterm-addon-fit';
+import { WebLinksAddon } from 'xterm-addon-web-links';
+
 import './App.css';
+import 'xterm/css/xterm.css';
+
+const getWebSocket = (): WebSocket => {
+  let socketURL;
+  let protocol;
+  const location = window.location;
+  protocol = location.protocol === 'https:' ? 'wss://' : 'ws://';
+  socketURL =
+    protocol +
+    location.hostname +
+    (location.port ? ':' + location.port : '') +
+    '/terminals/';
+  return new WebSocket(socketURL);
+};
 
 function App() {
+  useEffect(() => {
+    const term = new Terminal();
+    const attachAddon = new AttachAddon(getWebSocket());
+    const fitAddon = new FitAddon();
+    const webLinksAddon = new WebLinksAddon();
+    const searchAddon = new SearchAddon();
+
+    term.loadAddon(attachAddon);
+    term.loadAddon(fitAddon);
+    term.loadAddon(webLinksAddon);
+    term.loadAddon(searchAddon);
+    // term.write(`\x1b[H`);
+    term.open(document.getElementById('xterm-container') as HTMLElement);
+    fitAddon.fit();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={'xterm-container'} id={'xterm-container'}></div>
     </div>
   );
 }
